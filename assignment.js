d4ca1b1b630b99d37128340a354d430fc6932536
Gnana0792 <gnana.ravi18@gmail.com>
2026-07-08 22:12:03 +0530

    Add files via upload
    day1assignment

---
+++ assignment.js
@@ -0,0 +1,15 @@
+let firstName ="Gnanasekar"
+console.log (firstName);
+console.log (typeof firstName);
+let companyName ="Testleaf"
+console.log (companyName);
+console.log (typeof companyName);
+let mobileNumber = 9789884156
+console.log (mobileNumber)
+console.log (typeof mobileNumber)
+let isAutomation = true
+console.log (isAutomation)
+console.log (typeof isAutomation)
+let hasPlaywright
+console.log (hasPlaywright)
+console.log (typeof hasPlaywright)
